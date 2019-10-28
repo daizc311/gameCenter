f745ae73a192381a2dc45bdadaae7eba764971a9
Daizc <736233029@qq.com>
2019-10-28 18:52:25 +0800

    build(新增模块):
    主pom管理依赖

---
+++ game-center-model/src/main/java/club/dreamccc/gamecenter/model/GameTask.kt
@@ -0,0 +1,24 @@
+package club.dreamccc.gamecenter.model
+
+
+class GameTask {
+
+    var taskId: String? = null
+
+    var taskName: String? = null
+
+    var playerId: String? = null
+
+    var playerName: String? = null
+
+    var status: Double = 0.0
+
+    var itemList: List<GameTaskItem> = ArrayList()
+
+
+
+}
+
+data class GameTaskItem(var taskItemId: String, var taskItemName: String) {
+
+}
